56a211d530cd99c870af2806b3b95e827a3ad644
Radik Zangirov <owtmpatrick@gmail.com>
2023-01-25 15:29:14 +0500

    add running with docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import react from '@vitejs/plugin-react';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: ''
+    plugins: [reactRefresh()],
+    base: '',
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+    },
 });
